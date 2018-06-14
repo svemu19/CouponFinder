13a4bc434058a7e8e80e02a139249fcff60c0c4c
Sumedh Vemuganti <sumedhvemuganti@Sumedhs-MacBook-Pro.local>
2018-06-14 11:46:19 -0700

    second commit

---
+++ app/src/main/java/com/example/sumedhvemuganti/couponfinder/CameraActivity.java
@@ -0,0 +1,59 @@
+package com.example.sumedhvemuganti.couponfinder;
+
+import android.Manifest;
+import android.app.Activity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.*;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+public class CameraActivity extends Activity {
+
+protected void onCreate(Bundle bundle)
+{
+    super.onCreate(bundle);
+    setContentView(R.layout.camera_main);
+    System.out.print("Camera Activity");
+    final Context context= this;
+    if (checkCameraHardware(context))
+    {
+       // getCameraInstance();
+    }
+
+    android.hardware.camera2.CameraManager manager = (CameraManager) (this.getSystemService(Context.CAMERA_SERVICE));
+    try
+    {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            String[] arr = manager.getCameraIdList();
+            manager.openCamera(arr[0], null, null);
+        }
+
+    }
+    catch(Exception e)
+    {
+
+    }
+
+
+}
+
+
+
+
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+}
--- app/src/main/java/com/example/sumedhvemuganti/couponfinder/MainActivity.java
+++ app/src/main/java/com/example/sumedhvemuganti/couponfinder/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.sumedhvemuganti.couponfinder;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        final Context con = this;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent =  new Intent(con, CameraActivity.class);
+                startActivity(intent);
             }
         });
     }
